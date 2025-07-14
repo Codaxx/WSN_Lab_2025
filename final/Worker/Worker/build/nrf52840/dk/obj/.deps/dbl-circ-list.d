f13bdfd5d265c784effcefb3aa15253581c11a3a
chi xia <chi.xia@tumd.de>
2025-07-14 22:40:55 +0200

    new feature

---
+++ final/Worker/Worker/build/nrf52840/dk/obj/.deps/dbl-circ-list.d
@@ -0,0 +1,210 @@
+build/nrf52840/dk/obj/dbl-circ-list.o: \
+ /home/kkwsn/contiki-ng/os/lib/dbl-circ-list.c \
+ /home/kkwsn/contiki-ng/os/contiki.h \
+ /home/kkwsn/contiki-ng/arch/platform/nrf52840/./contiki-conf.h \
+ project-conf.h \
+ /home/kkwsn/contiki-ng/arch/platform/nrf52840/dk/nrf52840-board-def.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/boards/boards.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/hal/nrf_gpio.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/nrfx.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/integration/nrfx/nrfx_config.h \
+ /home/kkwsn/contiki-ng/arch/platform/nrf52840/config/sdk_config.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/drivers/nrfx_common.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf52840.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/core_cm4.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/cmsis_version.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/cmsis_compiler.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/cmsis_gcc.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/mpu_armv7.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/system_nrf52840.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/system_nrf.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf52840_bitfields.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/compiler_abstraction.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf_peripherals.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf52840_peripherals.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/integration/nrfx/nrfx_glue.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/integration/nrfx/legacy/apply_old_config.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/soc/nrfx_irqs.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/soc/nrfx_irqs_nrf52840.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/nrf_assert.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/app_util.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/compiler_abstraction.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/nordic_common.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/nordic_common.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/app_util_platform.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/nrf_assert.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/app_error.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/sdk_errors.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/app_error_weak.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/soc/nrfx_coredep.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/soc/nrfx_atomic.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/sdk_errors.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/sdk_resources.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/drivers/nrfx_errors.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/boards/pca10056.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/./nrf52840-def.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/cortex-m/cm4/cm4-def.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/./arm-def.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/./nrf52840-conf.h \
+ /home/kkwsn/contiki-ng/os/contiki-default-conf.h \
+ /home/kkwsn/contiki-ng/os/sys/process.h \
+ /home/kkwsn/contiki-ng/os/sys/pt.h /home/kkwsn/contiki-ng/os/sys/lc.h \
+ /home/kkwsn/contiki-ng/os/sys/lc-switch.h \
+ /home/kkwsn/contiki-ng/os/sys/cc.h \
+ /home/kkwsn/contiki-ng/os/sys/cc-gcc.h \
+ /home/kkwsn/contiki-ng/os/sys/autostart.h \
+ /home/kkwsn/contiki-ng/os/sys/process.h \
+ /home/kkwsn/contiki-ng/os/sys/timer.h \
+ /home/kkwsn/contiki-ng/os/sys/clock.h \
+ /home/kkwsn/contiki-ng/os/sys/ctimer.h \
+ /home/kkwsn/contiki-ng/os/sys/etimer.h \
+ /home/kkwsn/contiki-ng/os/sys/etimer.h \
+ /home/kkwsn/contiki-ng/os/sys/rtimer.h \
+ /home/kkwsn/contiki-ng/os/dev/watchdog.h \
+ /home/kkwsn/contiki-ng/arch/cpu/nrf52840/./rtimer-arch.h \
+ /home/kkwsn/contiki-ng/os/sys/pt.h /home/kkwsn/contiki-ng/os/sys/clock.h \
+ /home/kkwsn/contiki-ng/os/lib/dbl-circ-list.h
+
+/home/kkwsn/contiki-ng/os/contiki.h:
+
+/home/kkwsn/contiki-ng/arch/platform/nrf52840/./contiki-conf.h:
+
+project-conf.h:
+
+/home/kkwsn/contiki-ng/arch/platform/nrf52840/dk/nrf52840-board-def.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/boards/boards.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/hal/nrf_gpio.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/nrfx.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/integration/nrfx/nrfx_config.h:
+
+/home/kkwsn/contiki-ng/arch/platform/nrf52840/config/sdk_config.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/drivers/nrfx_common.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf52840.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/core_cm4.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/cmsis_version.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/cmsis_compiler.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/cmsis_gcc.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/CMSIS/CMSIS/Core/Include/mpu_armv7.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/system_nrf52840.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/system_nrf.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf52840_bitfields.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/compiler_abstraction.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf_peripherals.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/nrf52840_peripherals.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/integration/nrfx/nrfx_glue.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/integration/nrfx/legacy/apply_old_config.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/soc/nrfx_irqs.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/soc/nrfx_irqs_nrf52840.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/nrf_assert.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/app_util.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/mdk/compiler_abstraction.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/nordic_common.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/nordic_common.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/app_util_platform.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/nrf_assert.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/app_error.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/sdk_errors.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/app_error_weak.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/soc/nrfx_coredep.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/soc/nrfx_atomic.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/sdk_errors.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/libraries/util/sdk_resources.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/modules/nrfx/drivers/nrfx_errors.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/lib/nrf52-sdk/components/boards/pca10056.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/./nrf52840-def.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/cortex-m/cm4/cm4-def.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/../arm/./arm-def.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/./nrf52840-conf.h:
+
+/home/kkwsn/contiki-ng/os/contiki-default-conf.h:
+
+/home/kkwsn/contiki-ng/os/sys/process.h:
+
+/home/kkwsn/contiki-ng/os/sys/pt.h:
+
+/home/kkwsn/contiki-ng/os/sys/lc.h:
+
+/home/kkwsn/contiki-ng/os/sys/lc-switch.h:
+
+/home/kkwsn/contiki-ng/os/sys/cc.h:
+
+/home/kkwsn/contiki-ng/os/sys/cc-gcc.h:
+
+/home/kkwsn/contiki-ng/os/sys/autostart.h:
+
+/home/kkwsn/contiki-ng/os/sys/process.h:
+
+/home/kkwsn/contiki-ng/os/sys/timer.h:
+
+/home/kkwsn/contiki-ng/os/sys/clock.h:
+
+/home/kkwsn/contiki-ng/os/sys/ctimer.h:
+
+/home/kkwsn/contiki-ng/os/sys/etimer.h:
+
+/home/kkwsn/contiki-ng/os/sys/etimer.h:
+
+/home/kkwsn/contiki-ng/os/sys/rtimer.h:
+
+/home/kkwsn/contiki-ng/os/dev/watchdog.h:
+
+/home/kkwsn/contiki-ng/arch/cpu/nrf52840/./rtimer-arch.h:
+
+/home/kkwsn/contiki-ng/os/sys/pt.h:
+
+/home/kkwsn/contiki-ng/os/sys/clock.h:
+
+/home/kkwsn/contiki-ng/os/lib/dbl-circ-list.h:
---
+++ final/Worker/Worker/build/nrf52840/dk/obj/.deps/syscalls.d
@@ -0,0 +1,2 @@
+build/nrf52840/dk/obj/syscalls.o: \
+ /home/kkwsn/contiki-ng/os/lib/newlib/syscalls.c
